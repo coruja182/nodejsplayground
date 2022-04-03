ae5813b063401c18a702d40ca5547480839fd14f
Luis Henrique Silveira da Rocha <henrique.srocha@gmail.com>
2022-04-03 21:44:40 +0200

    feat: wip server part of fullstack study

---
+++ server-fs-study/routes/users.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  const users = [{
+    id: 'user-id-1',
+    name: 'user-name-1'
+  },
+  {
+    id: 'user-id-2',
+    name: 'user-name-2'
+  }]
+
+  res.send(users);
+});
+
+module.exports = router;
