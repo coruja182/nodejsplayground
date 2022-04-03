7c2ec5aec9b5a53e1a8e8d158941b42a45c56b34
Luis Henrique Silveira da Rocha <henrique.srocha@gmail.com>
2022-04-03 23:33:45 +0200

    feat: docker compose now working for UI and server

--- server-fs-study/routes/users.js
+++ server-fs-study/routes/users.js
@@ -6,11 +6,14 @@ router.get('/', function (req, res, next) {
   const users = [{
     id: 'user-id-1',
     name: 'user-name-1'
-  },
-  {
+  }, {
     id: 'user-id-2',
     name: 'user-name-2'
-  }]
+  }, {
+    id: 'churrina-id',
+    name: 'Elisa Cantamessa'
+  },
+  ]
 
   res.send(users);
 });
