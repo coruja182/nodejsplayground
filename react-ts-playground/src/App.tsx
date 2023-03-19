e8eefec4b80ffbae4040657487471573b243482b
Luis Henrique Silveira da Rocha <henrique.srocha@gmail.com>
2023-03-19 11:20:11 +0100

    feat: example with react bootstrap form

---
+++ react-ts-playground/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { ExampleForm } from './components/example-form/ExampleForm';
+
+function App() {
+  return (
+    <div className="App container-fluid">
+      <ExampleForm />
+    </div>
+  );
+}
+
+export default App;
