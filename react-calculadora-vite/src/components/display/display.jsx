657858ac585345e7a026414728bbc8806ab8087a
Luis Henrique Silveira da Rocha <henrique.srocha@gmail.com>
2023-03-20 21:53:12 +0100

    chore: removed react calculadora vite

--- react-calculadora-vite/src/components/display/display.jsx
+++
@@ -1,9 +0,0 @@
-import './display.css'
-
-// Functional component Display.
-const Display = ({ valor }) => {
-    console.log('Display: ', valor)
-    return <div className='Display' >{valor || "0"}</div>
-}
-
-export default Display
